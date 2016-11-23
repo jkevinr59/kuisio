29ffa22e0a5eddd087c58f3a7b009e24fcdfc9f7
putriengon <putrifitriyani23@gmail.com>
2016-11-23 23:49:33 +0700

    session header

--- Kuisio/resources/views/header.blade.php
+++ Kuisio/resources/views/header.blade.php
@@ -33,32 +33,49 @@
             </li>
         </ul>
         <!-- /.navbar-top-links -->
-
-        <div class="navbar-default sidebar" role="navigation">
-            <div class="sidebar-nav navbar-collapse">
-                <ul class="nav" id="side-menu">
-
-                    <li>
-                        <a href='{!! url('/admin/dashboard'); !!}'><i class="fa fa-dashboard fa-fw"></i> Beranda</a>
-                    </li>
-                    <li>
-                        <a href='{!! url('/manajemensoal'); !!}'><i class="fa fa-edit fa-fw"></i> Manajemen Soal <span class="fa arrow"></span></a>
-                        <ul class="nav nav-second-level">                            
-                            <li>
-                                <a href='{!! url('/admin/unggah'); !!}'>Unggah Soal</a>
-                            </li>
-                            <li>
-                                <a href='data'>Daftar Soal</a>
-                            </li>
-                        </ul>
-                        <!-- /.nav-second-level -->
-                    </li>                                           
-                </ul>
-            </div>
-            <!-- /.sidebar-collapse -->
-        </div>
         <!-- /.navbar-static-side -->
     </nav>
+
+    <div class="navbar-collapse collapse" style="background-color:#0066AB; color:#FFFFFF; font-weight:bold;">
+        <div class="container">
+            <div  class= "nav navbar-nav navbar-right" >
+                <form name="roleform" id="roleform">
+                    <tr>
+                        <td align="right" colspan="2" >
+
+                        Selamat Datang Putri Nur Fitriyani
+
+                        </td>
+                    </tr>
+                </form>         
+            </div>
+        </div>
+    </div>
+
+    <div class="navbar-default sidebar" role="navigation">
+        <div class="sidebar-nav navbar-collapse">
+            <ul class="nav" id="side-menu">
+
+                <li>
+                    <a href='{!! url('/admin/dashboard'); !!}'><i class="fa fa-dashboard fa-fw"></i> Beranda</a>
+                </li>
+                <li>
+                    <a href='{!! url('/manajemensoal'); !!}'><i class="fa fa-edit fa-fw"></i> Manajemen Soal <span class="fa arrow"></span></a>
+                    <ul class="nav nav-second-level">                            
+                        <li>
+                            <a href='{!! url('/admin/unggah'); !!}'>Unggah Soal</a>
+                        </li>
+                        <li>
+                            <a href='data'>Daftar Soal</a>
+                        </li>
+                    </ul>
+                    <!-- /.nav-second-level -->
+                </li>                                           
+            </ul>
+        </div>
+        <!-- /.sidebar-collapse -->
+    </div>
+    
 </div>
 <!-- /#wrapper -->
 
